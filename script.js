19d633e1bfdc5c0311dd9d1903bfea9267f86784
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-10-09 12:55:44 +0200

    Finally worked out the grid created functions to keep the drawing board an equal width and height

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ let container = document.createElement('div');
 container.className = 'container';
 document.body.appendChild(container);
 
-function whiteboard() {
-    let numberOfDivs = 256;
-    for (let n = 0; n < numberOfDivs; n++) {
+function whiteboard(numDiv) {
+        for (let n = 0; n < (numDiv * numDiv); n++) {
         const pixel = document.createElement('div');
         pixel.className = 'pixel';
+        pixel.addEventListener('mouseover', function(){
+            pixel.style.background = 'black'
+        })
         container.appendChild(pixel);
     } 
 }
 
-let pixel = whiteboard();
+function adjustBoard(gridSize) {
+    container.style.maxWidth = `${10 * gridSize}px`
+}
 
-pixel.onmouseover =  console.log('EYYY')
+boardSize = 100
+
+pixels = whiteboard(boardSize);
+adjustBoard(boardSize)
