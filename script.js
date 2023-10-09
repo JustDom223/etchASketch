7a436fe03cc516186561a092c0fe0f9f354689a5
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-10-09 17:58:16 +0200

    I have started again using HTML for the static parts and shifting the whiteboard to be the sole part that is in JS i beleve it was overcomplicated before

--- script.js
+++ script.js
@@ -1,23 +1,83 @@
-let container = document.createElement('div');
-container.className = 'container';
-document.body.appendChild(container);
+const whiteBoard = document.getElementById('whiteBoard')
+const eraseButton = document.getElementById('eraseButton')
+const blackButton = document.getElementById('blackButton')
+const rainbowButton = document.getElementById('rainbowButton')
 
-function whiteboard(numDiv) {
-        for (let n = 0; n < (numDiv * numDiv); n++) {
-        const pixel = document.createElement('div');
-        pixel.className = 'pixel';
-        pixel.addEventListener('mouseover', function(){
-            pixel.style.background = 'black'
-        })
-        container.appendChild(pixel);
-    } 
+
+// variables 
+let gridSize = 156
+
+
+
+// Function to create a whiteboard with a specified number of pixels
+function createWhiteboard(numDiv) {
+  for (let n = 0; n < numDiv * numDiv; n++) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixel.style.width = `${500 / numDiv}px`
+    pixel.style.height = `${500 / numDiv}px`
+  
+
+    // Add a mouseover event listener to change the background to a random color
+    pixel.addEventListener('mouseover', function () {
+      // let randomColor = getRandomColor();
+      pixel.style.background = 'black' //randomColor;
+    });
+
+    whiteBoard.appendChild(pixel);
+  }
 }
 
-function adjustBoard(gridSize) {
-    container.style.maxWidth = `${10 * gridSize}px`
-}
 
-boardSize = 100
 
-pixels = whiteboard(boardSize);
-adjustBoard(boardSize)
+
+
+
+createWhiteboard(gridSize)
+
+
+// // Create a button and place it in the top of the 
+// const generateBoardButton = document.createElement('button')
+// generateBoardButton.className = 'generateBoardButton'
+// generateBoardButton.textContent = 'Click to change grid size'
+// generateBoardButton.addEventListener('click', gridSizeButton)
+// document.body.appendChild(generateBoardButton)
+
+
+
+// // Create a 
+
+// // Create a whiteboard div and append it to the body
+// const whiteboard = document.getElementById('whiteboard')
+// whiteboard.className = 'whiteboard';
+// document.body.appendChild(whiteboard);
+
+
+// // Function to adjust the size of the grid
+// function gridSizeButton(){
+//     boardSize = prompt('What size grid would you like? eg. 10 = 10*10')
+//     createWhiteboard(boardSize)
+//     adjustBoardSize(boardSize)
+// }
+
+
+// // Function to generate a random hex color code
+// function getRandomColor() {
+//   const letters = '0123456789ABCDEF';
+//   let color = '#';
+  
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+  
+//   return color;
+// }
+
+// // Function to adjust the board size
+// function adjustBoardSize(gridSize) {
+//   whiteboard.style.maxWidth = `${10 * gridSize}px`;
+// }
+
+// let boardSize = 16;
+// createWhiteboard(boardSize);
+// adjustBoardSize(boardSize);
