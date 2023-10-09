29b9c5123230b212ec26e1d52eb9b5c699d3cb16
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-10-09 09:50:16 +0200

    Created the relevant file Beginning the foundations for the sketch board

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # etchASketch
+This is my version of the etch a sketch
---
+++ script.js
@@ -0,0 +1,12 @@
+let container = document.createElement('div')
+container.id = 'container'
+container.textContent('this is a test')
+
+
+
+document.body.appendChild(container)
+
+
+function createPad() {
+
+}
